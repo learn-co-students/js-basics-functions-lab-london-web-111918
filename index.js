98cd6568010f68d33e288090aba1dc61ae14715d
IMH51 <ianmartinharrison@outlook.com>
2019-01-05 17:38:17 +0000

    Completed js basics functions lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+  if (pickup <= 42) {
+    blocks = 42 - pickup
+  } else {
+    blocks = pickup - 42
+  }
+  return blocks
+}
+
+function distanceFromHqInFeet(pickup) {
+  return distanceFromHqInBlocks(pickup) * 264
+}
+
+function distanceTravelledInFeet(start_block, end_block) {
+  if (start_block <= end_block) {
+    distance = end_block - start_block
+  } else {
+    distance = start_block - end_block
+  }
+  return distance * 264
+}
+
+function calculatesFarePrice(start, destination) {
+  distance = distanceTravelledInFeet(start, destination)
+  if (distance < 400) {
+    fare = 0
+  } else if (distance < 2000){
+    fare = (distance - 400) * 0.02
+  } else if (distance < 2500) {
+    fare = 25
+  } else {
+    fare = "cannot travel that far"
+  }
+  return fare
+}
