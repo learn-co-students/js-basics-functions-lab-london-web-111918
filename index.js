f22746aa152652635ddba256af333ee2302baf69
NathanB09 <nathanbell09@gmail.com>
2019-01-06 11:23:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  const hq = 42;
+  return block > hq ? block - hq : hq - block;
+}
+
+function distanceFromHqInFeet(block) {
+  let blocks = distanceFromHqInBlocks(block);
+  return blocks * 264;
+}
+
+function distanceTravelledInFeet(from, to) {
+  let blockDistance = to > from ? to - from : from - to;
+  return blockDistance * 264;
+}
+
+function calculatesFarePrice(from, to) {
+  let distanceFeet = distanceTravelledInFeet(from, to);
+  switch (true) {
+    case distanceFeet > 2500:
+      return 'cannot travel that far';
+    case distanceFeet > 2000:
+      return 25;
+    case distanceFeet < 400:
+      return 0;
+    case distanceFeet < 2000:
+      return (distanceFeet - 400) * 0.02;
+  }
+}
